98f544b51083b7c596c3f2fdac9366241323aaef
Julian Theoderik Trajanson <julian.trajanson@gmail.com>
2021-11-15 23:30:04 -0500

    learning react query - chat room useQuery

--- src/api/queries/useGetChatRooms.ts
+++ src/api/queries/useGetChatRooms.ts
@@ -2,9 +2,14 @@ import { useQuery } from "react-query";
 import { CacheKeys } from "#/contexts/queryClient";
 import { Api } from "..";
 
-export const useGetChatRooms = () => {
-  return useQuery([CacheKeys.chatRooms], async () => {
-    const res = await Api.getPageOfChatRooms({ pageSize: 5 });
+export interface GetChatRoomsArgs {
+  cursor?: string;
+}
+
+
+export const useGetChatRooms = ({ cursor }: GetChatRoomsArgs) => {
+  return useQuery([CacheKeys.chatRooms, cursor], async () => {
+    const res = await Api.getPageOfChatRooms({ cursor, pageSize: 5 });
     return res.data;
   });
 };
