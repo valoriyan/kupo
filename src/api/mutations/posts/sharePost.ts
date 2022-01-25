0a7d10d7538054c76aaca64b6d2385778f590d96
Julian Theoderik Trajanson <julian.trajanson@gmail.com>
2022-01-25 10:49:50 -0500

    share posts api

--- src/api/mutations/posts/sharePost.ts
+++ src/api/mutations/posts/sharePost.ts
@@ -1,20 +1,23 @@
 import { useMutation } from "react-query";
 import Router from "next/router";
-import { useFormState } from "#/templates/AddContent/FormContext";
 import { Api } from "../..";
 
-export const useCreatePost = () => {
-  const formState = useFormState();
-
+export const useSharePost = ({
+  sharedPostId,
+  caption,
+  hashtags,
+}: {
+  sharedPostId: string;
+  caption: string;
+  hashtags: string[];
+}) => {
   return useMutation(
     async () => {
-      return await Api.createPost(
-        formState.mediaFiles.map((media) => media.file),
-        formState.caption,
-        JSON.stringify(formState.hashTags),
-        formState.publicationDate?.valueOf().toString(),
-        formState.expirationDate?.valueOf().toString(),
-      );
+      return await Api.sharePost({
+        sharedPostId,
+        caption,
+        hashtags,
+      });
     },
     {
       onSuccess: (data) => {
