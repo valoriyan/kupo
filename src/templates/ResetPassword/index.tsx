7edbf4d99c9be18a63226b385cd3ffd8c0206ee8
Julian Theoderik Trajanson <julian.trajanson@gmail.com>
2022-05-13 17:24:42 -0400

    reset password flow

--- src/templates/ResetPassword/index.tsx
+++ src/templates/ResetPassword/index.tsx
@@ -1,7 +1,12 @@
 import { useRouter } from "next/router";
-import { FormEventHandler, useEffect } from "react";
+import { FormEventHandler, useEffect, useState } from "react";
 import { useCheckResetPasswordTokenValidity } from "#/api/mutations/users/checkResetPasswordTokenValidity";
 import { AuthFormLayout } from "#/components/AuthFormLayout";
+import { Stack } from "#/components/Layout";
+import { Input } from "#/components/Input";
+import { Button } from "#/components/Button";
+import { useResetPassword } from "#/api/mutations/users/resetPassword";
+import { TextOrSpinner } from "#/components/TextOrSpinner";
 
 export const ResetPassword = () => {
   const router = useRouter();
@@ -16,6 +21,10 @@ export const ResetPassword = () => {
 };
 
 const ResetPasswordInner = ({ resetPasswordToken }: { resetPasswordToken: string }) => {
+  const [password, setPassword] = useState("");
+
+  const { mutateAsync: resetPassword, isLoading } = useResetPassword();
+
   const {
     mutateAsync: checkResetPasswordTokenValidity,
     data: isTokenValid,
@@ -34,11 +43,28 @@ const ResetPasswordInner = ({ resetPasswordToken }: { resetPasswordToken: string
 
   const onSubmit: FormEventHandler<HTMLFormElement> = (e) => {
     e.preventDefault();
+    console.log("CALLING", password);
+    resetPassword({ password, token: resetPasswordToken });
   };
 
   return (
     <AuthFormLayout title="Reset Password" onSubmit={onSubmit}>
-      <div>Reset Password Here</div>
+      <Stack css={{ gap: "$5" }}>
+        <Input
+          size="lg"
+          required
+          type="password"
+          placeholder="new password"
+          name="password"
+          value={password}
+          onChange={(e) => setPassword(e.currentTarget.value)}
+        />
+      </Stack>
+      <Stack css={{ gap: "$5", pt: "$10" }}>
+        <Button size="lg" variant="primary" disabled={isLoading} type="submit">
+          <TextOrSpinner isLoading={isLoading}>Reset Password</TextOrSpinner>
+        </Button>
+      </Stack>
     </AuthFormLayout>
   );
 };
