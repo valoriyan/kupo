8462abd343aacf27d4f6c5b074989228e7ae86d5
Julian Theoderik Trajanson <julian.trajanson@gmail.com>
2022-05-15 11:48:47 -0400

    deploy

--- src/controllers/utilities/shareTypesWithFrontendController.ts
+++ src/controllers/utilities/shareTypesWithFrontendController.ts
@@ -1,19 +1,22 @@
 import { Controller, Post, Route, Body } from "tsoa";
-import { UnrenderableCanceledCommentOnPostNotification, UnrenderableCanceledNewFollowerNotification, UnrenderableCanceledNewLikeOnPostNotification } from "../notification/models/unrenderableCanceledUserNotifications";
+import {
+  UnrenderableCanceledCommentOnPostNotification,
+  UnrenderableCanceledNewFollowerNotification,
+  UnrenderableCanceledNewLikeOnPostNotification,
+} from "../notification/models/unrenderableCanceledUserNotifications";
 
 @Route("utilities")
 export class ShareTypesWithFrontendController extends Controller {
-
-    @Post("sendDataTypesToFrontend1")
-    public async sendDataTypesToFrontend1(
-      @Body() requestBody: {
-          UnrenderableCanceledCommentOnPostNotification: UnrenderableCanceledCommentOnPostNotification;
-          UnrenderableCanceledNewFollowerNotification: UnrenderableCanceledNewFollowerNotification;
-          UnrenderableCanceledNewLikeOnPostNotification: UnrenderableCanceledNewLikeOnPostNotification;
-        },
-    ): Promise<{response: string;}> {
-        console.log(requestBody);
-        return {response: "response"}
-    }  
-
+  @Post("sendDataTypesToFrontend1")
+  public async sendDataTypesToFrontend1(
+    @Body()
+    requestBody: {
+      UnrenderableCanceledCommentOnPostNotification: UnrenderableCanceledCommentOnPostNotification;
+      UnrenderableCanceledNewFollowerNotification: UnrenderableCanceledNewFollowerNotification;
+      UnrenderableCanceledNewLikeOnPostNotification: UnrenderableCanceledNewLikeOnPostNotification;
+    },
+  ): Promise<{ response: string }> {
+    console.log(requestBody);
+    return { response: "response" };
+  }
 }
