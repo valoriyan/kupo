d0f847b54ecc50ac7479774c68661163d1102226
Blake Zimmerman <blake.zimmerman@icloud.com>
2022-08-09 19:40:19 -0400

    Discover page bug fix

--- src/templates/Discover/SearchResults/PostResults.tsx
+++ src/templates/Discover/SearchResults/PostResults.tsx
@@ -67,7 +67,9 @@ const PostPreview = ({ post }: { post: RenderablePost }) => {
 
   return (
     <PostWrapper onClick={() => goToPostPage(post.id)}>
-      {!!post.mediaElements[0] && <PostImage src={post.mediaElements[0].temporaryUrl} />}
+      {!!post.mediaElements?.[0] && (
+        <PostImage src={post.mediaElements[0].temporaryUrl} />
+      )}
       <Stack css={{ gap: "$4", px: "$5", py: "$5" }}>
         <Flex css={{ alignItems: "center", gap: "$4" }}>
           <UserName username={user?.username} />
