0f03bb327cc8b18172b1e4d1d87efb56087e58d2
Blake Zimmerman <blake.zimmerman@icloud.com>
2023-01-01 17:02:09 -0500

    Fix build error

--- frontend/src/components/Input/index.tsx
+++ frontend/src/components/Input/index.tsx
@@ -1,6 +1,6 @@
 import { useId } from "@radix-ui/react-id";
 import { forwardRef } from "react";
-import { styled } from "#/styling";
+import { CSS, styled } from "#/styling";
 import { Stack } from "../Layout";
 import { Body, Subtext } from "../Typography";
 
@@ -9,22 +9,23 @@ type InputAttributes = JSX.IntrinsicElements["input"];
 export interface InputProps extends Omit<InputAttributes, "size"> {
   size?: "md" | "lg";
   label?: string;
+  css?: CSS;
 }
 
 export const Input = forwardRef<HTMLInputElement, InputProps>(
-  ({ size, label, id, ...props }, ref) => {
+  ({ size, label, css, id, ...props }, ref) => {
     const generatedId = useId();
     const effectiveId = id ?? generatedId;
 
     const inputNode = (
       // For some reason TypeScript has a problem with the type of our ref
       // eslint-disable-next-line @typescript-eslint/no-explicit-any
-      <InputElement ref={ref as any} id={effectiveId} size={size} {...props} />
+      <InputElement ref={ref as any} id={effectiveId} size={size} css={css} {...props} />
     );
 
     if (label) {
       return (
-        <Stack as="label" htmlFor={effectiveId} css={{ gap: "$2" }}>
+        <Stack as="label" htmlFor={effectiveId} css={{ gap: "$2", ...css }}>
           {size === "lg" ? (
             <Body css={{ color: "$secondaryText", pl: "$4" }}>{label}</Body>
           ) : (
--- frontend/src/components/TextArea/index.tsx
+++ frontend/src/components/TextArea/index.tsx
@@ -1,6 +1,6 @@
 import { useId } from "@radix-ui/react-id";
 import { forwardRef } from "react";
-import { styled } from "#/styling";
+import { CSS, styled } from "#/styling";
 import { Stack } from "../Layout";
 import { Body, Subtext } from "../Typography";
 
@@ -9,22 +9,29 @@ type TextAreaAttributes = JSX.IntrinsicElements["textarea"];
 export interface TextAreaProps extends Omit<TextAreaAttributes, "size"> {
   label?: string;
   size?: "md" | "lg";
+  css?: CSS;
 }
 
 export const TextArea = forwardRef<HTMLTextAreaElement, TextAreaProps>(
-  ({ size, label, id, ...props }, ref) => {
+  ({ size, label, css, id, ...props }, ref) => {
     const generatedId = useId();
     const effectiveId = id ?? generatedId;
 
     const textAreaNode = (
-      // For some reason TypeScript has a problem with the type of our ref
-      // eslint-disable-next-line @typescript-eslint/no-explicit-any
-      <TextAreaElement ref={ref as any} id={effectiveId} size={size} {...props} />
+      <TextAreaElement
+        // For some reason TypeScript has a problem with the type of our ref
+        // eslint-disable-next-line @typescript-eslint/no-explicit-any
+        ref={ref as any}
+        id={effectiveId}
+        size={size}
+        css={css}
+        {...props}
+      />
     );
 
     if (label) {
       return (
-        <Stack as="label" htmlFor={effectiveId} css={{ gap: "$2" }}>
+        <Stack as="label" htmlFor={effectiveId} css={{ gap: "$2", ...css }}>
           {size === "lg" ? (
             <Body css={{ color: "$secondaryText", pl: "$4" }}>{label}</Body>
           ) : (
