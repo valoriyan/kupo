99ce192ed705e9de4760330d70952ebad926c19d
Blake Zimmerman <blake.zimmerman@icloud.com>
2023-03-23 00:07:08 -0400

    Use correct DB query for user autocomplete

--- backend/src/controllers/user/handleAutoCompleteUsername.ts
+++ backend/src/controllers/user/handleAutoCompleteUsername.ts
@@ -44,7 +44,7 @@ export async function handleAutoCompleteUsername({
   //////////////////////////////////////////////////
 
   const selectUsersByUsernameMatchingSubstringResponse =
-    await controller.databaseService.tableNameToServicesMap.usersTableService.selectUsersByUsernameMatchingSubstring(
+    await controller.databaseService.tableNameToServicesMap.usersTableService.selectOrderedUsersByUsernameMatchingSubstring(
       {
         controller,
         usernameSubstring: searchString.toLowerCase(),
